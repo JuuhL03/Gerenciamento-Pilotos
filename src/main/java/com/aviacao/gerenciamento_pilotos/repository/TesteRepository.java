8a71df9c32d414072fc62b3013e09be6961fcec1
Júllia Santos <jullialopesgomes@gmail.com>
2025-12-29 20:52:45 -0300

    correção

--- src/main/java/com/aviacao/gerenciamento_pilotos/repository/TesteRepository.java
+++ src/main/java/com/aviacao/gerenciamento_pilotos/repository/TesteRepository.java
@@ -5,6 +5,7 @@ import com.aviacao.gerenciamento_pilotos.domain.enums.StatusTeste;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,8 +16,8 @@ public interface TesteRepository extends JpaRepository<Teste, Long>, JpaSpecific
 
     List<Teste> findByAlunoIdOrderByIdDesc(Long alunoId);
 
-    @Query("SELECT t FROM Teste t WHERE t.aluno.id = :alunoId ORDER BY t.id DESC LIMIT 1")
-    Optional<Teste> findTesteAtualByAlunoId(Long alunoId);
+    @Query(value = "SELECT * FROM teste WHERE aluno_id = :alunoId ORDER BY id DESC LIMIT 1", nativeQuery = true)
+    Optional<Teste> findTesteAtualByAlunoId(@Param("alunoId") Long alunoId);
 
     List<Teste> findByStatus(StatusTeste status);
 
