b5904f67a2478390488e740319d8a74d8d45774f
Júllia Santos <jullialopesgomes@gmail.com>
2026-01-14 14:37:53 -0300

    ALTERAÇÃO DE CARGOS

--- src/main/java/com/aviacao/gerenciamento_pilotos/domain/enums/Cargo.java
+++ src/main/java/com/aviacao/gerenciamento_pilotos/domain/enums/Cargo.java
@@ -1,7 +1,6 @@
 package com.aviacao.gerenciamento_pilotos.domain.enums;
 
 public enum Cargo {
-    ADMIN,      // Administrador - acesso total
-    INSTRUTOR,  // Instrutor - pode gerenciar alunos e testes
-    OPERADOR    // Operador - apenas leitura
+    ADMIN,
+    INSTRUTOR
 }
